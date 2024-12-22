b6dd8aab7dff3933ea7cfa4a11b823ddf5b892b4
reynand <rocso58@gmail.com>
2024-12-22 11:47:38 +0800

    Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AdminComponent } from "./components/admin/admin.component";
+import { UserComponent } from './components/user/user.component';
+import { SingleComponentComponent } from "./components/single-component/single-component.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AdminComponent, UserComponent, SingleComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/single-component/single-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-component',
+  imports: [],
+  template: "<h1 class='text-color'>Hi, This is single component</h1>",
+  styles:['.text-color {color:blue}']
+})
+export class SingleComponentComponent {
+
+}
